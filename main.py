19ffc015f5221bb3a5baed0833ef2ce99dc81ddd
i3Cheese <dan.sot.2003@gmail.com>
2020-02-24 20:04:53 +0700

    Add reaction to button press
    Signed-off-by: i3Cheese <dan.sot.2003@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.Qt import QPixmap, QImage
+from PyQt5.Qt import QPixmap, QImage, Qt
 
 from ui_window import Ui_MainWindow
 
@@ -14,8 +15,8 @@ class MainWindow(QWidget, Ui_MainWindow):
         super().__init__()
         super().setupUi(self)
         self.static_api_params = {'ll': '37.589434,55.734088',
-                                  'z': '15',
-                                  'l': 'map',
+                                  'z': 2,
+                                  'l': 'sat',
                                   'size': '450,450'}
         self.update_image()
 
@@ -30,6 +31,42 @@ class MainWindow(QWidget, Ui_MainWindow):
             # Set image
             img = QPixmap.fromImage(QImage.fromData(response.content))
             self.map_container.setPixmap(img)
+
+    def move_map(self, dx, dy):
+        x, y = map(float, self.static_api_params["ll"].split(','))
+        move_delta = 360 / (2 ** self.static_api_params["z"])
+        x = (x + move_delta * dx) % 180
+        if x > 90:
+            x -= 180
+        y = (y + move_delta * dy) % 180
+        if y > 90:
+            y -= 180
+        print(x, y)
+        self.static_api_params["ll"] = '%f,%f' % (x, y)
+        self.update_image()
+
+    def change_scale(self, d: int):
+        # d - {1, -1}
+        z = self.static_api_params["z"]
+        z += d
+        if not (0 <= z <= 17):
+            return
+        self.static_api_params["z"] = z
+        self.update_image()
+
+    def keyPressEvent(self, a0: QtGui.QKeyEvent) -> None:
+        if a0.key() == Qt.Key_PageUp:
+            self.change_scale(1)
+        elif a0.key() == Qt.Key_PageDown:
+            self.change_scale(-1)
+        elif a0.key() == Qt.Key_Up:
+            self.move_map(0, 1)
+        elif a0.key() == Qt.Key_Down:
+            self.move_map(0, -1)
+        elif a0.key() == Qt.Key_Left:
+            self.move_map(-1, 0)
+        elif a0.key() == Qt.Key_Right:
+            self.move_map(1, 0)
 
 
 if __name__ == '__main__':
