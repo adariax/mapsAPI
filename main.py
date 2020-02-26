48b0cf7e9a4c07018607963eb80b59f7a2be9af9
Daxa <apelsinKDA@gmail.com>
2020-02-26 22:02:07 +0700

    Update interface, add toponym search

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class MainWindow(QWidget, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         super().setupUi(self)
-        self.static_api_params = {'ll': '37.589434,55.734088',
+        self.static_api_params = {'ll': '37.588392,55.734036',
                                   'z': 13,
                                   'l': 'map',
                                   'size': '450,450'}
@@ -25,8 +25,7 @@ class MainWindow(QWidget, Ui_MainWindow):
         self.cb_trf.stateChanged.connect(self.change_layouts)
         self.cb_skl.stateChanged.connect(self.change_layouts)
 
-        # Redirect focus from buttons
-        self.setFocusPolicy(Qt.TabFocus)
+        self.bt_search.clicked.connect(self.search_by_button)
 
         self.update_image()
 
@@ -74,6 +73,50 @@ class MainWindow(QWidget, Ui_MainWindow):
         self.static_api_params["l"] = ','.join(layouts)
         self.update_image()
 
+    def search_by_button(self):
+        toponym_to_find = self.le_search.text()
+
+        # Make search by organization and geocoder
+
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+        search_api_server = "https://search-maps.yandex.ru/v1/"
+
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": toponym_to_find,
+            "format": "json"}
+        search_params = {
+            "apikey": "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3",
+            "text": toponym_to_find,
+            "lang": 'ru_RU'}
+
+        geocoder_response = requests.get(geocoder_api_server, params=geocoder_params)
+        search_response = requests.get(search_api_server, params=search_params)
+
+        if not geocoder_response:
+            return
+
+        geocoder_response_json = geocoder_response.json()
+        search_response_json = search_response.json()
+
+        # Get list of toponyms for checking finding toponyms
+        geocoder_toponyms = geocoder_response_json["response"]["GeoObjectCollection"] \
+            ["featureMember"]
+        # Check if the Search by Org. couldn't found
+        if search_response_json['properties']['ResponseMetaData']['SearchRequest']['results']:
+            toponym_coordinates = ','.join(map(str, search_response_json['features'][0] \
+                                               ["geometry"]["coordinates"]))
+        elif len(geocoder_toponyms):
+            toponym_coordinates = geocoder_toponyms[0]["GeoObject"] \
+                ["Point"]["pos"].replace(' ', ',')
+        else:
+            return  # Exit if the toponym weren't found
+
+        self.static_api_params['ll'] = toponym_coordinates  # Update coords of founded toponym
+        self.static_api_params['pt'] = f'{toponym_coordinates},org'  # Make a point on the map
+
+        self.update_image()
+
     def keyPressEvent(self, a0: QtGui.QKeyEvent) -> None:
         if a0.key() == Qt.Key_PageUp:
             self.change_scale(1)
@@ -87,6 +130,8 @@ class MainWindow(QWidget, Ui_MainWindow):
             self.move_map(-1, 0)
         elif a0.key() == Qt.Key_Right:
             self.move_map(1, 0)
+        elif a0.key() == Qt.Key_Enter:
+            self.map_container.setFocus()
 
 
 if __name__ == '__main__':
